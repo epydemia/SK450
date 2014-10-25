b0be6b1fb42dd5049d652a121c880e2b7804f9fd
epydemia <epydemia@gmail.com>
2014-10-25 20:34:40 +0200

    19Ottobre2014
    MultiWii 2.3 - SK450 Standard
    MultiwiiConf
    MultiWiiGUI

---
+++ MultiWii/dc.h
@@ -0,0 +1,4 @@
+    motor[0] = PIDMIX(-1,+1.08,-1); //REAR_R
+    motor[1] = PIDMIX(-1.31,-0.56,+1.31); //FRONT_R
+    motor[2] = PIDMIX(+1,+1.08,+1); //REAR_L
+    motor[3] = PIDMIX(+1.31,-0.56,-1.31); //FRONT_L
